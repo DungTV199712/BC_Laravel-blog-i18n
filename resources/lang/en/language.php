7229c1f3f095b7b054a963567154967bfc238911
DungTV <anhdunngcoi@gmail.com>
2018-11-30 08:39:21 +0700

    upload files

---
+++ resources/lang/en/language.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dung
+ * Date: 29/11/2018
+ * Time: 18:06
+ */
+return [
+    'blog_application' => 'Blog Application',
+    'home' => 'Home',
+    'list_post' => 'Post list',
+    'create_post' => 'Create Post',
+    'save' => 'Save',
+    'post_not_exist' => 'Post not exist',
+    'title' => 'Title',
+    'content' => 'Content',
+];
---
+++ resources/lang/vi/language.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dung
+ * Date: 29/11/2018
+ * Time: 18:06
+ */
+return [
+    'blog_application' => 'Ứng dụng Blog',
+    'home' => 'Trang chủ',
+    'list_post' => 'Danh Sách Bài Viết',
+    'create_post' => 'Tạo Bài Viết',
+    'save' => 'Lưu',
+    'post_not_exist' => 'Không có bài viết nào',
+    'title' => 'Tiêu đề',
+    'content' => 'Nội dung',
+];
